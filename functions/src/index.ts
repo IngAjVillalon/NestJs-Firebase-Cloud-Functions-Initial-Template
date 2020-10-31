6895c2899fa0533bf0182f3f94ff9b5ff35d5a1f
Ing. Alejandro Javier Villalón Navarro <ing.ajvillalon@gmail.com>
2020-10-30 23:47:55 -0600

    convection corrections

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@
 import * as functions from 'firebase-functions';
 
 // Function Import
-import { NestServer } from "./nestApi/nest-server";
+import { NestServer } from "./nest-api/nest-server";
 
 // Functions Implementation
 export const api = functions.runWith({ memory: '2GB', timeoutSeconds: 150 }).https.onRequest(NestServer);
