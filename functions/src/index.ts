dec18a2fe0157a9f344ffef1bdc7123ec14ed1df
Ing. Alejandro Javier Villalón Navarro <ing.ajvillalon@gmail.com>
2020-10-30 18:53:41 -0600

    dependencies updated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,5 @@ import * as functions from 'firebase-functions';
 // Function Import
 import { NestServer } from "./nestApi/nest-server";
 
-
 // Functions Implementation
 export const api = functions.runWith({ memory: '2GB', timeoutSeconds: 150 }).https.onRequest(NestServer);
